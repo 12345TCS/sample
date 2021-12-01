44357419540805a67db474b927db39fd86a9f689
DezyNation <87603422+DezyNation@users.noreply.github.com>
2021-12-01 15:36:32 +0530

    Add files via upload

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,15 +1,20 @@
-const manuBarOne = document.getElementById("rect1043");
-const manuBarTwo = document.getElementById("rect1147");
 var menuClicks = 0;
+var swidth = screen.width;
 
 function openMenu(){
     menuClicks++;
     if(menuClicks % 2 != 0){
         document.getElementById("rect1043").style.transform = "rotate(45deg) translateX(5px) translateY(2px)";
         document.getElementById("rect1147").style.transform = "rotate(-45deg) translateX(13px) translateY(2px)";
+        document.getElementById("side-menu").style.left = "0";
     }
     else{
         document.getElementById("rect1043").style.transform = "unset";
         document.getElementById("rect1147").style.transform = "translateX(2.125rem)";
+        if (swidth <= 768) {
+            document.getElementById("side-menu").style.left = "-80vw";   
+        } else {
+            document.getElementById("side-menu").style.left = "-20vw";
+        }
     }
 }
