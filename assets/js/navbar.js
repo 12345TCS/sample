7a4af72edb170db94495b29eb910d42ef4e83a89
DezyNation <87603422+DezyNation@users.noreply.github.com>
2021-12-02 22:33:19 +0530

    Add files via upload

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,9 +1,16 @@
+let rect1043 = document.getElementById("rect1043");
+let rect1147 = document.getElementById("rect1147");
 var menuClicks = 0;
 var swidth = screen.width;
 
 function openMenu(){
     menuClicks++;
     if(menuClicks % 2 != 0){
+        document.getElementById("rect1043").style.fill = "#000";
+        document.getElementById("rect1147").style.fill = "#000";
+        if (swidth <= 768) {
+            document.getElementById("brandnum").style.color = "#000";  
+        }
         document.getElementById("rect1043").style.transform = "rotate(45deg) translateX(5px) translateY(2px)";
         document.getElementById("rect1147").style.transform = "rotate(-45deg) translateX(13px) translateY(2px)";
         document.getElementById("side-menu").style.left = "0";
@@ -11,6 +18,9 @@ function openMenu(){
     else{
         document.getElementById("rect1043").style.transform = "unset";
         document.getElementById("rect1147").style.transform = "translateX(2.125rem)";
+        document.getElementById("rect1043").style.fill = "#FFF";
+        document.getElementById("rect1147").style.fill = "#FFF";
+        document.getElementById("brandnum").style.color = "#f7c327ff";
         if (swidth <= 768) {
             document.getElementById("side-menu").style.left = "-80vw";   
         } else {
