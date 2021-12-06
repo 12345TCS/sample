869327282d9efb939a7b05b6312cab89425b28bf
DezyNation <87603422+DezyNation@users.noreply.github.com>
2021-12-06 16:09:28 +0530

    Add files via upload

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -14,13 +14,15 @@ function openMenu(){
         document.getElementById("rect1043").style.transform = "rotate(45deg) translateX(5px) translateY(2px)";
         document.getElementById("rect1147").style.transform = "rotate(-45deg) translateX(13px) translateY(2px)";
         document.getElementById("side-menu").style.left = "0";
+        blank.style.top = "0";
     }
     else{
         document.getElementById("rect1043").style.transform = "unset";
         document.getElementById("rect1147").style.transform = "translateX(2.125rem)";
-        document.getElementById("rect1043").style.fill = "#FFF";
-        document.getElementById("rect1147").style.fill = "#FFF";
+        document.getElementById("rect1043").style.fill = "#f7c327ff";
+        document.getElementById("rect1147").style.fill = "#f7c327ff";
         document.getElementById("brandnum").style.color = "#f7c327ff";
+        blank.style.top = "-100vh";
         if (swidth <= 768) {
             document.getElementById("side-menu").style.left = "-80vw";   
         } else {
@@ -33,9 +35,10 @@ blank.addEventListener("click", ()=>{
     if (menuClicks %2 != 0) {
         document.getElementById("rect1043").style.transform = "unset";
         document.getElementById("rect1147").style.transform = "translateX(2.125rem)";
-        document.getElementById("rect1043").style.fill = "#FFF";
-        document.getElementById("rect1147").style.fill = "#FFF";
+        document.getElementById("rect1043").style.fill = "#f7c327ff";
+        document.getElementById("rect1147").style.fill = "#f7c327ff";
         document.getElementById("brandnum").style.color = "#f7c327ff";
+        blank.style.top = "-100vh";
         if (swidth <= 768) {
             document.getElementById("side-menu").style.left = "-80vw";   
         } else {
